57457341302aa2a9a724f0dadbb553426db36111
zhowiny <278774750@qq.com>
2018-12-19 10:28:07 +0800

    修改部分bug

--- src/pages/qrcode/index.vue
+++ src/pages/qrcode/index.vue
@@ -43,7 +43,7 @@
       wx.getSystemInfo({
         success (e) {
           width = e.windowWidth - 40
-          height = e.windowHeight - 80
+          height = e.windowHeight - 40
         }
       })
       return {
