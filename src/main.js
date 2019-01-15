8c071c92ab5882b9399361f3f3e3538adaea5e92
zhowiny <278774750@qq.com>
2019-01-15 10:20:53 +0800

    zhowiny

--- src/main.js
+++ src/main.js
@@ -50,6 +50,7 @@ Vue.mixin({
           fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (('00' + o[k]).substr(('' + o[k]).length)))
         }
       }
+      
       return fmt
     }
   }
